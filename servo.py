1737abd440f465741931bace677270694ba1057b
VinuraDeS1997 <141820960+VinuraDeS1997@users.noreply.github.com>
2024-05-31 17:40:23 +0800

    Create servo.py

---
+++ servo.py
@@ -0,0 +1,24 @@
+[servo.py]
+import RPi.GPIO as GPIO
+from time import sleep
+# import COMConfig import data
+GPIO.setmode(GPIO.BOARD)
+
+class Servo:
+    def __init__(self, pin, data_rate):
+        self.pin = pin
+        self.data_rate = data_rate
+        #Setup of Servo
+        GPIO.setup(self.pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.pin, self.data_rate)
+        self.pwm.start(0)
+    #Sets the angle of servo
+    def setAngle(self, angle):
+        duty = angle / 18 + 2
+        GPIO.output(self.pin, True)
+        self.pwm.ChangeDutyCycle(duty)
+        sleep(1)
+        GPIO.output(self.pin, False)
+        self.pwm.ChangeDutyCycle(duty)
+
+
