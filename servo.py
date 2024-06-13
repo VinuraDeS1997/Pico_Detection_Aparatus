6691e8ed9e0307a046f4dd0355416d565f116d1c
VinuraDeS1997 <141820960+VinuraDeS1997@users.noreply.github.com>
2024-06-13 11:32:53 +0800

    Update servo.py
    Added Docstrings

--- servo.py
+++ servo.py
@@ -1,3 +1,6 @@
+"""
+Module for controlling a servo motor using Raspberry Pi GPIO.
+"""
 #servo.py
 import RPi.GPIO as GPIO
 from time import sleep
@@ -5,7 +8,32 @@ GPIO.setmode(GPIO.BOARD)
 
 #Define class
 class Servo:
+"""
+    A class to represent a servo motor.
+    
+    Attributes
+    ----------
+    pin : int
+        The GPIO pin number where the servo is connected.
+    data_rate : int
+        The data rate for PWM signal.
+
+    Methods
+    -------
+    set_angle(angle)
+        Sets the angle of the servo motor.
+"""
     def __init__(self, pin, data_rate):
+"""
+        Constructs all the necessary attributes for the servo object.
+
+        Parameters
+        ----------
+        pin : int
+            The GPIO pin number where the servo is connected.
+        data_rate : int
+            The data rate for PWM signal.
+"""
         self.pin = pin
         self.data_rate = data_rate
         #Setup of Servo
@@ -15,6 +43,14 @@ class Servo:
         
     #Sets the angle of servo
     def setAngle(self, angle):
+"""
+        Sets the angle of the servo motor.
+
+        Parameters
+        ----------
+        angle : float
+            The angle to set the servo to.
+"""
         duty = angle / 18 + 2
         GPIO.output(self.pin, True)
         self.pwm.ChangeDutyCycle(duty)
