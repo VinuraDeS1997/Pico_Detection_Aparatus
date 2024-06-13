b8b73a150d9860c02c889ab298444c264755b375
VinuraDeS1997 <141820960+VinuraDeS1997@users.noreply.github.com>
2024-06-13 11:52:42 +0800

    Update servo.py
    Adjusted comments and made code more cleaner

--- servo.py
+++ servo.py
@@ -1,62 +1,49 @@
 """
 Module for controlling a servo motor using Raspberry Pi GPIO.
 """
-#servo.py
+
 import RPi.GPIO as GPIO
 from time import sleep
+
 GPIO.setmode(GPIO.BOARD)
 
-#Define class.
-class Servo:
-"""
-    A class to represent a servo motor.
-    
-    Attributes
-    ----------
-    pin : int
-        The GPIO pin number where the servo is connected.
-    data_rate : int
-        The data rate for PWM signal.
 
-    Methods
-    -------
-    set_angle(angle)
-        Sets the angle of the servo motor.
-"""
+class Servo:
+    """
+    A class to represent a servo motor.
+
+    Attributes:
+        pin (int): The GPIO pin number where the servo is connected.
+        data_rate (int): The data rate for PWM signal.
+    """
 
     def __init__(self, pin, data_rate):
-"""
+        """
         Constructs all the necessary attributes for the servo object.
 
-        Parameters
-        ----------
-        pin : int
-            The GPIO pin number where the servo is connected.
-        data_rate : int
-            The data rate for PWM signal.
-"""
+        Args:
+            pin (int): The GPIO pin number where the servo is connected.
+            data_rate (int): The data rate for PWM signal.
+        """
         self.pin = pin
         self.data_rate = data_rate
-        #Setup of Servo
+        # Setup of Servo
         GPIO.setup(self.pin, GPIO.OUT)
         self.pwm = GPIO.PWM(self.pin, self.data_rate)
         self.pwm.start(0)
-        
-    
-    def setAngle(self, angle): #Sets the angle of servo.
-"""
+
+    def set_angle(self, angle):
+        """
         Sets the angle of the servo motor.
 
-        Parameters
-        ----------
-        angle : float
-            The angle to set the servo to.
-"""
-        duty = angle / 18 + 2
-        GPIO.output(self.pin, True)
+        Args:
+            angle (float): The angle to set the servo to.
+        """
+        duty = angle / 18 + 2  # Convert angle to duty cycle
+        GPIO.output(self.pin, True)  # Enable PWM signal
         self.pwm.ChangeDutyCycle(duty)
-        sleep(1)
-        GPIO.output(self.pin, False)
-        self.pwm.ChangeDutyCycle(duty)
+        sleep(1)  # Wait for servo to move
+        GPIO.output(self.pin, False)  # Disable PWM signal
+        self.pwm.ChangeDutyCycle(duty)  # Keep the duty cycle constant
 
 
