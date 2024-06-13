2f6c25392b873b89d1aee37b2e3cbf1af266e6a4
VinuraDeS1997 <141820960+VinuraDeS1997@users.noreply.github.com>
2024-06-13 11:53:44 +0800

    Update test.py
    Adjusted comments and made code cleaner

--- test.py
+++ test.py
@@ -1,37 +1,35 @@
 """
 Script to control the angle of a servo motor with Raspberry Pi.
 """
-#test.py
-#Control the angle of a Servo Motor with Raspberry Pi.
 
-#initialising imports from other files.
+# Initialising components
 import RPi.GPIO as GPIO
 from time import sleep
 from servo import Servo
 from PiPico import Pico
 
-
 GPIO.setmode(GPIO.BOARD)
 
-#Number A is referencing what pins each servo is connected to whereas number Hz.
-Servo_A = Servo(11, 50)
-Servo_B = Servo(16, 50)
+# Initialize servos
+servo_a = Servo(11, 50)
+servo_b = Servo(16, 50)
 count = 0
-numLoops = 2
+num_loops = 2
 
-Serial = Pico('/dev/ttyACM0')
+serial = Pico('/dev/ttyACM0')
 
-#Determining which servo does which angle.
-"""
-
-Main loop to read data and set servo angles accordingly.
-
-"""
 while True:
-    Pivot_x, Pivot_y = Pico.data(Serial)
-    Servo.setAngle(Servo_A, Pivot_x)
-    Servo.setAngle(Servo_B, Pivot_y)
-    sleep(0.5)
+    """
+    Main loop to read data and set servo angles accordingly.
+    """
+    # Get data from Pico
+    pivot_x, pivot_y = Pico.data(serial)
+    
+    # Set servo angles
+    servo_a.set_angle(pivot_x)
+    servo_b.set_angle(pivot_y)
+    
+    sleep(0.5)  # Wait before reading the next set of data
     
 
 
