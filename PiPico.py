46abbd9d08eaad00171eaf56d00777729ec193fa
VinuraDeS1997 <141820960+VinuraDeS1997@users.noreply.github.com>
2024-06-13 10:50:00 +0800

    Update PiPico.py
    Added comments/changed the '&' to 'and' to make sure that there was no bugs

--- PiPico.py
+++ PiPico.py
@@ -1,15 +1,19 @@
 [PiPico.py]
+#Import functions from what file you're trying to read the data for the pyserial from the serial port
 import serial
+#Define class
 class Pico:
+    #initialize instance of Pico set up a serial connection to the specified port
     def __init__(self, port):
         self.port = serial.Serial(port)
-        
+     #Defines coordinates of X and Y as whole numbers
     def data(self):
         data = self.port.readline().decode('utf-8').strip()
         x, y = data.split()
         x = int(x.split('.')[0])
         y = int(y.split('.')[0])
-        if y > 0 & y < 135:
+        #Check if 'y' is between 0 and 135
+        if y > 0 and y < 135:
             return x, y
         
         
