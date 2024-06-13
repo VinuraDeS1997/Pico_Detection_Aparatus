c635a09cce876469348fec5517c33393d98d1ea7
VinuraDeS1997 <141820960+VinuraDeS1997@users.noreply.github.com>
2024-06-13 11:51:33 +0800

    Update COMConfig.py
    Adjusted Comments and made code more cleaner

--- COMConfig.py
+++ COMConfig.py
@@ -1,27 +1,25 @@
 """
 Module for configuring and reading data from a serial port.
 """
-#COMConfig.py
-# Windows PC (Python)
-#Import functions from the python file of which you're trying to transfer the data from
+
 import serial
 
 # Configure the serial port
 ser = serial.Serial('/dev/ttyACM0')  # Replace 'COM3' with the appropriate port
 
-"""
-    Main loop to read data from the serial port and print it.
-"""
 while True:
+    """
+    Main loop to read data from the serial port and print it.
+    """
     # Read data from the serial port
     data = ser.readline().decode('utf-8').strip()
-    print(data)    # Check if data was received
-    
+    print(data)  # Print the received data
+
     if data:
         # Split the data into x and y values
         x, y = data.split(',')
-        x = int(x.split('.')[0])
-        y = int(y.split('.')[0])
+        x = int(x.split('.')[0])  # Convert x value to integer
+        y = int(y.split('.')[0])  # Convert y value to integer
 
         # Print the received values
         print(f"Received: x={x}, y={y}")
